2718f3ba09b230371585a19ad5fed2e22702a62d
Pathompong Thitithan <the.midnight.k.0173@gmail.com>
2023-10-15 03:28:32 +0700

    update page

--- DAY-17-Midterm/partials/navbar.js
+++ DAY-17-Midterm/partials/navbar.js
@@ -57,48 +57,4 @@ const navbarHtml = `
 </nav>
 `;
 
-const footerHtml = `
-<div class="px-4">
-  <div class="container">
-    <footer class="pt-5">
-      <div class="row">
-        <div class="col col-md-6 mb-3">
-          <h5>Section</h5>
-          <ul class="nav row-cols-2">
-            <li class="nav-item col mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
-            <li class="nav-item col mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
-            <li class="nav-item col mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
-            <li class="nav-item col mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-            <li class="nav-item col mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
-          </ul>
-        </div>
-
-        <div class="col-md-5 offset-md-1 mb-3">
-          <form>
-            <h5>Subscribe to our newsletter</h5>
-            <p>Monthly digest of what's new and exciting from us.</p>
-            <div class="d-flex flex-column flex-sm-row w-100 gap-2 gap-sm-3">
-              <label for="newsletter1" class="visually-hidden">Email address</label>
-              <input id="newsletter1" type="text" class="form-control bg-dark-subtle rounded-pill px-4 border-0" placeholder="Email address">
-              <button class="btn btn-outline-light rounded-pill px-4" type="button">Subscribe</button>
-            </div>
-          </form>
-        </div>
-      </div>
-
-      <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-        <p>© 2023 Company, Inc. All rights reserved.</p>
-        <ul class="list-unstyled d-flex">
-          <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
-          <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
-          <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
-        </ul>
-      </div>
-    </footer>
-  </div>
-</div>
-`;
-
-// insert navbarHtml and footerHtml into the body
 document.body.insertAdjacentHTML("afterbegin", navbarHtml);
-document.body.insertAdjacentHTML("beforeend", footerHtml);
