d88683637b8e576e860e545444a8cf6e84118a5e
Pathompong Thitithan <the.midnight.k.0173@gmail.com>
2023-10-28 03:50:39 +0700

    update

--- DAY-21/vite.config.ts
+++ DAY-21/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/BorntoDev-FEBC-Homeworks/DAY-21/',
+  base: '/DAY-21/build/',
 })
