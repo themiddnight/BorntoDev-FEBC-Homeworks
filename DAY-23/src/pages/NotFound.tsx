e486c4420ac888ef76413513668a1d7e1f48e62f
Pathompong Thitithan <the.midnight.k.0173@gmail.com>
2023-12-23 14:29:50 +0700

    Refactor variable names for clarity

--- DAY-23/src/pages/NotFound.tsx
+++ DAY-23/src/pages/NotFound.tsx
@@ -1,10 +1,16 @@
+import { Box, Typography } from "@mui/material"
+import Navbar from "../components/Navbar"
+
 function NotFound() {
-  return (
-    <div>
-      <h1>404</h1>
-      <p>Not Found</p>
-    </div>
-  )
+    return (
+        <>
+            <Navbar />
+            <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' pt={9}>
+                <Typography variant='h3'>404</Typography>
+                <Typography variant='h4'>Page Not Found</Typography>
+            </Box>
+        </>
+    )
 }
 
 export default NotFound
